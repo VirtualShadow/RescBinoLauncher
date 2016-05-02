b14cae51d8ba0bae7963e79a615d4f75f56adb7f
Virtualshadow <virtualshadowmask@gmail.com>
2016-05-03 02:42:50 +0800

    change1

---
+++ app/src/main/java/cn/virtualshadow/resc/Activity/ClearDataActivity.java
@@ -0,0 +1,17 @@
+package cn.virtualshadow.resc.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import cn.virtualshadow.resc.R;
+
+public class ClearDataActivity extends BlankActivity
+{
+
+	@Override
+	protected void start(Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		super.start(savedInstanceState);
+		
+	}
+	
+}
