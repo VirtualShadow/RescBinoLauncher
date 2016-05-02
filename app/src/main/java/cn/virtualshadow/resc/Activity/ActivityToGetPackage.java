7580508c392a669c97fd7b4bd51ffc4dae84f945
VirtualShadow <virtualshadowmask@gmail.com>
2016-05-02 11:39:17 +0800

    $two

---
+++ app/src/main/java/cn/virtualshadow/resc/Activity/ActivityToGetPackage.java
@@ -0,0 +1,15 @@
+package cn.virtualshadow.resc.Activity;
+import android.os.Bundle;
+
+public class ActivityToGetPackage extends BlankActivity
+{
+
+	@Override
+	protected void start(Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		super.start(savedInstanceState);
+		
+	}
+	
+}
---
+++ app/src/main/java/cn/virtualshadow/resc/AppDataManager/AppData.java
@@ -0,0 +1,11 @@
+package cn.virtualshadow.resc.AppDataManager;
+import android.graphics.drawable.Drawable;
+
+public class AppData
+{
+	CharSequence packageName;
+	CharSequence appName;
+	Drawable drawable;
+	String permission;
+	
+}
