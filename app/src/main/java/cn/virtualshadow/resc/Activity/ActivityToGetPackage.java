29d847733ef873344ce4bdc6510e19471def880c
Virtualshadow <virtualshadowmask@gmail.com>
2016-05-03 23:35:24 +0800

    change3

--- app/src/main/java/cn/virtualshadow/resc/Activity/ActivityToGetPackage.java
+++ app/src/main/java/cn/virtualshadow/resc/Activity/ActivityToGetPackage.java
@@ -7,11 +7,12 @@ import cn.virtualshadow.resc.AppDataManager.AppData;
 import cn.virtualshadow.resc.util.DataStream;
 import java.util.ArrayList;
 import java.util.List;
+import android.widget.Toast;
 
 public class ActivityToGetPackage extends BlankActivity
 {
 	private List<ResolveInfo> appList;
-	
+
 	private PackageManager packageMamager;
 
 	private ArrayList<AppData> appDataList=new ArrayList<AppData>();
@@ -22,31 +23,47 @@ public class ActivityToGetPackage extends BlankActivity
 		super.start(savedInstanceState);
 		getApps();
 	} 
-	
-	private void getApps(){
-		packageMamager=getPackageManager();
-		Intent intent=new Intent(Intent.ACTION_MAIN,null);
+
+	private void getApps()
+	{
+		packageMamager = getPackageManager();
+		Intent intent=new Intent(Intent.ACTION_MAIN, null);
 		intent.addCategory(Intent.CATEGORY_LAUNCHER);
-		appList=packageMamager.queryIntentActivities(intent,0);
-		try{
-			for(ResolveInfo resolve:appList){
-				AppData appData=new AppData();
-				appData.appName=(String) resolve.loadLabel(packageMamager);
-				appData.packageName=resolve.activityInfo.packageName;
-				appData.icon=resolve.loadIcon(packageMamager);
-				appData.permission=resolve.activityInfo.permission;
-				appDataList.add(appData);
-			}
-		}finally{
-			showApps();
+		appList = packageMamager.queryIntentActivities(intent, 0);
+		if (appList.size() == (getFilesDir().listFiles().length / 2))
+		{
+			//缓存数量与app数量相等
+			Toast.makeText(this, "同步完成", Toast.LENGTH_LONG).show();
 		}
-		
+		else
+		{
+			//app数量与缓存数量不相符
+			try
+			{
+				for (ResolveInfo resolve:appList)
+				{
+					AppData appData=new AppData();
+					appData.appName = (String) resolve.loadLabel(packageMamager);
+					appData.packageName = resolve.activityInfo.packageName;
+					appData.icon = resolve.loadIcon(packageMamager);
+					appData.permission = resolve.activityInfo.permission;
+					appDataList.add(appData);
+				}
+			}
+			finally
+			{
+
+				showApps();
+				Toast.makeText(this, "更新完毕", Toast.LENGTH_LONG).show();
+			}
+		}
+
 	}
 
 	private void showApps()
 	{
-		// TODO: Implement this method
-		DataStream.push(this,appDataList).to();
+		// 重写缓存
+		DataStream.push(this, appDataList).to();
 	}
-	
+
 }
